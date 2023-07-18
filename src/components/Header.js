9a4a7063a2d2c743d476bd8dfbc9ffc641e1b59e
RichardChileya <richardchileyawk@gmail.com>
2023-07-18 22:36:05 +0200

    Add Header file

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Header = () => (
+  <header className="header">
+    <h1 className="logo">AAAZ Tracker</h1>
+    <nav className="nav-bar">
+      <ul className="nav-list">
+        <li className="nav-item">Dashboard</li>
+        <li className="nav-item">Attendance</li>
+        <li className="nav-item">Requests</li>
+      </ul>
+    </nav>
+  </header>
+);
+
+export default Header;

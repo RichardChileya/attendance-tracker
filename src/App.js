874acaf2f61331606bdf6bf7c42882cc3d5ea3d9
RichardChileya <richardchileyawk@gmail.com>
2023-07-19 22:26:35 +0200

    Add header to App Js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-
+        <Header />
         <h1 className="lets-react">Lets React</h1>
       </header>
     </div>

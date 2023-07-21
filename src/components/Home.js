68e021e1a6392ee272805d76e94a8206597f8ccf
RichardChileya <richardchileyawk@gmail.com>
2023-07-21 22:39:00 +0200

    Add checkin buttons

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,8 +3,12 @@ import React from 'react';
 const Home = () => (
   <>
     <div className="page-hero">
-      <h1> Lets Get To Work</h1>
       <img src="https://source.unsplash.com/800x400/?teamwork" alt="team work" />
+    </div>
+    <div className="checkin">
+      <button className="time-btn" type="button"> Check In</button>
+      <button className="time-btn" type="button"> Lunch </button>
+      <button className="time-btn" type="button"> Checkout </button>
     </div>
   </>
 );

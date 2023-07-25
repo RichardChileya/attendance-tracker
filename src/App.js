28a0d215c404a5c3f9c41f81c9176c66aa211120
RichardChileya <richardchileyawk@gmail.com>
2023-07-25 21:33:58 +0200

    Update App JS

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
+import AppRouter from './Router';
 import Header from './components/Header';
-import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Header />
-        <Home />
+        <AppRouter />
       </header>
     </div>
   );

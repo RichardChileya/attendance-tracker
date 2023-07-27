a1c08c9e3fe03ac57784ce22d368df8e0f9054c1
RichardChileya <richardchileyawk@gmail.com>
2023-07-27 21:29:22 +0200

    Add configStore

--- src/redux/ConfigStore.js
+++ src/redux/ConfigStore.js
@@ -1,15 +1,20 @@
-import { combineReducers, getDefaultMiddleware } from "@reduxjs/toolkit";
-import React from "react";
+import { combineReducers, configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import logger from 'redux-logger';
+import userSlice from './reducer/userSlice';
+import attendanceSlice from './reducer/attendanceSlice';
+import requestSlice from './reducer/requestSlice';
 
 const rootReducer = combineReducers({
-  dashboard: dashboardSlie, 
+  user: userSlice,
+  requests: requestSlice,
   attendances: attendanceSlice,
 });
 
-const store = () => (
-{
-  reducer: rootReducer, middleware: [...getDefaultMiddleware(), logger],
-},
+const store = configureStore(
+  {
+    reducer: rootReducer,
+    middleware: [...getDefaultMiddleware(), logger],
+  },
 );
- 
+
 export default store;
